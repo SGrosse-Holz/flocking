ac3c02e13eda5da176723fe41dad926ac552f14d
Simon Grosse-Holz <sgh256@mit.edu>
2019-12-10 23:27:03 -0500

    update equilibration

--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -153,7 +153,7 @@ alternating (leapfrog) pattern. Thus the symmetric formulation of a
 	System system(initial_state, &integrator);
 
 	// system.get_state().dt = 1e-4;
-	system.step(1000000);
+	system.step(10000000);
 	// system.get_state().dt = dt;
 
 	if (report_states)
